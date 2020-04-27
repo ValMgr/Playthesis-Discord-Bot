9273a0a3cfb5e991d3aca85e0c30397578908190
ValMgz <valou32720@gmail.com>
2020-04-27 14:17:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NzAwMDk5NTQwNDAwNzM0MjI5.XpeFjw.qJtm24qRdXLaIh4tmfRurG4zldQ';
+
+const prefix = "!";
+const serverID = '689422588660809730';
+const channelID = '700107648527237221';
+
+var lab;
+client.login(token);
+
